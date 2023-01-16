6a9fdff2d076e703d1ae7834a0032a75a246a70c
rey@m307.uc-bcf.edu.ph <rey@m307.uc-bcf.edu.ph>
2023-01-17 00:29:40 +0800

    18

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html lang = "en">
+       <head>
+            <meta charset ="UTF-8">
+            <meta http-equiv ="X-UA-Compatible" content="IE=edge">
+            <meta name = "viewport" content="width-device-width, initial-scale-1.0">
+            <title> About </title>
+        </head>
+        <body>
+            <h3>About Page</h3>
+            <p>The number of works of popular culture that feature Humpty Dumpty are a testament to the character’s and the rhyme’s popularity. 
+                Most prominently, as mentioned above, the character features and Lewis Carroll’s Through the Looking Glass. Within this text,
+                Alice and Humpty Dumpty discuss his shape, and egg, and the fact that although he looks like an egg he is not one. </p>
+        </body>
+    </html>           
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html lang = "en">
+       <head>
+            <meta charset ="UTF-8">
+            <meta http-equiv ="X-UA-Compatible" content="IE=edge">
+            <meta name = "viewport" content="width-device-width, initial-scale-1.0">
+            <title>Contact Us</title>
+        </head>
+        <body>
+            <h3>Contact Page</h3>
+            <p>Robert Frost is one of the most famous poets from the 1900s. He never earned a formal college degree, 
+                but he did receive honorary degrees from more than 40 colleges and universities. This famous poem shows 
+                that everything in life is cyclical and that the beauty in nature only lasts for a short period of time.
+                 Even though life ends, there is new life waiting to come forth. </p>
+        </body>
+    </html>           
